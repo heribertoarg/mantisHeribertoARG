98595e3138191b08a90e5a8b82784d01e71ed51f
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-06 08:04:18 -0700

    [Studio] Add screen "createreport"

---
+++ client/controllers/app/createreport.js
@@ -0,0 +1,5 @@
+angular.module('app').controller('app_createreport', app_createreport);
+function app_createreport($scope, app) {
+    'use strict';
+    app.init($scope);
+}
--- client/states.js
+++ client/states.js
@@ -60,5 +60,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.createreport', {
+        views: {
+            app: {
+                controller: 'app_createreport',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.createreport');
+                }
+            }
+        }
     });
 });
