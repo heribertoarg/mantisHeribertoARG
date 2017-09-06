d18b64513f51265625c7dd02f549157178afec6b
Heriberto Adolfo Rodriguez Guzman <heribertoadolfo.rodriguezguzman@flex.com>
2017-09-06 12:22:19 -0700

    [Studio] Modified screen "createreport"

--- client/controllers/app/createreport.js
+++ client/controllers/app/createreport.js
@@ -1,5 +1,7 @@
 angular.module('app').controller('app_createreport', app_createreport);
 function app_createreport($scope, app) {
     'use strict';
-    app.init($scope);
+    app.init($scope, function() {
+        $scope.data.category.options=JSON.parse($scope.data.category.options);
+    });
 }
